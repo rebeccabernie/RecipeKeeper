159dd50dae37ccf35c46af1abccf3edbd1ddbc6d
rebeccabernie <rebecca.kane@outlook.com>
2016-11-26 23:22:10 +0000

    Now creates 3 additional tables, add dummy data to each

--- createDatabase.py
+++ createDatabase.py
@@ -9,12 +9,24 @@ def setup_db():
     db = sqlite3.connect(DATABASE) # Create connection object, represents database. Data stored in DATABASE, defined above
     cur = db.cursor() # Allows SQL commands to be performed
 
-    # Create the table if it doesn't exist
-    cur.execute("CREATE TABLE IF NOT EXISTS recipetable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
+    # Create the tables if they don't exist
+    # Breakfast Table = btable
+    cur.execute("CREATE TABLE IF NOT EXISTS btable(name TEXT PRIMARY KEY, ingredients TEXT, method TEXT)")
+    # Lunch Table = ltable
+    cur.execute("CREATE TABLE IF NOT EXISTS ltable(name TEXT PRIMARY KEY, ingredients TEXT, method TEXT)")
+    # Dinner Table = dtable
+    cur.execute("CREATE TABLE IF NOT EXISTS dtable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
+    # Sweet/Dessert Table = stable
+    cur.execute("CREATE TABLE IF NOT EXISTS stable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
+    
     db.commit() # Save changes
     
-    # Insert example into database
-    cur.execute("INSERT INTO recipetable VALUES('Pancakes', 'Eggs, Milk, SF Flour', 'Add all to bowl and mix.')")
+    # Insert examples into tables
+    cur.execute("INSERT INTO btable VALUES('Pancakes', 'Eggs, Milk, SF Flour', 'Add all to bowl and mix.')")
+    cur.execute("INSERT INTO ltable VALUES('Cheese Sandwich', 'Bread, Butter, Cheese', 'Butter bread, add cheese')")
+    cur.execute("INSERT INTO dtable VALUES('Roast Chicken', 'Whole Chicken, Butter, Salt, Pepper', 'Put butter, salt and pepper on chicken')")
+    cur.execute("INSERT INTO stable VALUES('Cupcake', 'Eggs, Sugar, Marg, SF Flour', 'Add all to bowl and mix.')")
+    
     db.commit()
     db.close() # Close database
 
