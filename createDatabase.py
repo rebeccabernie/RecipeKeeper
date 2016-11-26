3a7b0a137e6775cb19e70833c806e8f64549e0cc
rebeccabernie <rebecca.kane@outlook.com>
2016-11-26 22:49:04 +0000

    Correctly adds dummy data to DB is table is empty

--- createDatabase.py
+++ createDatabase.py
@@ -5,21 +5,18 @@ import sqlite3
 DATABASE = 'recipes.db' # Sets DATABASE to recipes file in subfolder
 
 def setup_db():
-  # Create database if it doesn't exist
-  db = sqlite3.connect(DATABASE) # Create connection object, represents database. Data stored in DATABASE, defined above
-  cur = db.cursor() # Allows SQL commands to be performed
+    # Create database if it doesn't exist
+    db = sqlite3.connect(DATABASE) # Create connection object, represents database. Data stored in DATABASE, defined above
+    cur = db.cursor() # Allows SQL commands to be performed
 
-  # Create the table if it doesn't exist
-  cur.execute("CREATE TABLE IF NOT EXISTS recipetable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
-  db.commit() # Save changes
-
-  cur.execute("SELECT COUNT(*) FROM recipetable")
-  if cur.fetchall() == 0: # If all columns are empty, insert following information
+    # Create the table if it doesn't exist
+    cur.execute("CREATE TABLE IF NOT EXISTS recipetable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
+    db.commit() # Save changes
+    
+    # Insert example into database
     cur.execute("INSERT INTO recipetable VALUES('Pancakes', 'Eggs, Milk, SF Flour', 'Add all to bowl and mix.')")
     db.commit()
-    
-    #test = cur.execute("SELECT * FROM recipetable")
-    print(str(cur.fetchall()))
+    db.close() # Close database
 
 if __name__ == "__main__":
   setup_db()
