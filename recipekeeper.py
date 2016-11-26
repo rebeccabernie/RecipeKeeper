84548760c2cefb140d8f3f18ddeb33f7f4a78cbe
rebeccabernie <rebecca.kane@outlook.com>
2016-11-26 23:48:25 +0000

    saves name/ing/method to breakfast table

--- recipekeeper.py
+++ recipekeeper.py
@@ -4,21 +4,40 @@ import sqlite3
 DATABASE = 'recipes/recipes.db'
 app = fl.Flask(__name__)
 
-# Load database
-def get_db():
+# Load database, adapted from https://github.com/data-representation/example-sqlite
+def loadDB():
   db = getattr(fl.g, '_database', None)
   if db is None:
     db = fl.g._database = sqlite3.connect(DATABASE)
-  return db
+    return db
 
 @app.route("/")
 def root():
-    return app.send_static_file('index.html')
+    return app.send_static_file('index.html') # Load index.html file
 
-@app.route("/bfast", methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
 def bfast():
-	bfast = fl.request.values["rname"]
-	return rname
+	rname = fl.request.values["rname"]
+    ing = fl.request.values["ing"]
+    methpd = fl.request.values["method"]
+	
+    db = sqlite3.connect(DATABASE) # Create connection object, represents database. Data stored in DATABASE, defined above
+    cur = db.cursor() # Allows SQL commands to be performed
+
+    # Formatting for /VALUES(%s), var/ found at http://stackoverflow.com/questions/902408/how-to-use-variables-in-sql-statement-in-python
+    cur.execute("INSERT INTO btable VALUES(%s, %s, %s)", rname, ing, method)
+    
+    db.commit()
+    
+    return rname
+
+@app.route("/saved", methods=["GET", "POST"])
+def savedRecipes():
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    
+    cur.execute("SELECT rname FROM btable")
+    return str(cur.fetchall())
 
 if __name__ == "__main__":
     app.run()
