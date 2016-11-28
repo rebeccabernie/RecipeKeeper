671630a854ea0f2d010b42c775894569a21166cc
rebeccabernie <rebecca.kane@outlook.com>
2016-11-28 15:54:18 +0000

    Added render_template functionality

--- recipekeeper.py
+++ recipekeeper.py
@@ -1,4 +1,5 @@
 import flask as fl
+from flask import render_template, request, g
 import sqlite3
 
 DATABASE = 'recipes.db'
@@ -11,35 +12,38 @@ def loadDB():
     db = fl.g._database = sqlite3.connect('recipes.db')
     return db
 
+@app.teardown_appcontext
+def close_connection(exception):
+  db = getattr(fl.g, '_database', None)
+  if db is not None:
+    db.close()
+
 @app.route("/")
 def root():
-    return app.send_static_file('index.html') # Load index.html file
+    return render_template('index.html') # Load index.html file
 
 @app.route("/add", methods=["GET", "POST"])
 def addRecipe():
-    rname = fl.request.values["rname"]
-    ing = fl.request.values["ing"]
-    method = fl.request.values["method"]
-
-    db = sqlite3.connect('recipes.db') # Create connection object, represents database. Data stored in DATABASE, defined above
-    cur = db.cursor() # Allows SQL commands to be performed
-
+    cur = loadDB().cursor()
+    conn = sqlite3.connect(DATABASE)
+    curConn = conn.cursor()
+    
     # Formatting for /VALUES(%s), var/ found at http://stackoverflow.com/questions/902408/how-to-use-variables-in-sql-statement-in-python
-    cur.execute("INSERT INTO btable VALUES(?, ?, ?)", (rname, ing, method))
+    cur.execute("INSERT INTO btable(rname, ing, method) VALUES(?, ?, ?)",(fl.request.form['rname'],fl.request.form['ing'], fl.request.form['method']))   
 
-    db.commit()
-    db.close() # Close database
-
-    return rname
+    conn.commit()
+    conn.close() # Close database
+    
+    render_template('basicTemplate.html') + str(cur.fetchall())
 
 @app.route("/savedRecipes", methods=["GET", "POST"])
 def savedRec():
-    db = sqlite3.connect('recipes.db')
-    cur = db.cursor()
-    
+    cur = loadDB().cursor()    
+    cur.execute("Select * from btable")
     cur.execute("SELECT rname FROM btable")
     db.close() # Close database
-    return str(cur.fetchall())
+    
+    return (render_template('basicTemplate.html') + str(cur.fetchall()))
 
 if __name__ == "__main__":
     app.run()
