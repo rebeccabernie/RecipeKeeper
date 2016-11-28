40f455b6e933c3fa526c32bd0c6dae69422da7a5
rebeccabernie <rebecca.kane@outlook.com>
2016-11-28 13:49:54 +0000

    Temporarily working with just one table for simplicity purposes

--- createDatabase.py
+++ createDatabase.py
@@ -11,21 +11,21 @@ def setup_db():
 
     # Create the tables if they don't exist
     # Breakfast Table = btable
-    cur.execute("CREATE TABLE IF NOT EXISTS btable(name TEXT PRIMARY KEY, ingredients TEXT, method TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS btable(rname TEXT, ing TEXT, method TEXT, PRIMARY KEY (rname))")
     # Lunch Table = ltable
-    cur.execute("CREATE TABLE IF NOT EXISTS ltable(name TEXT PRIMARY KEY, ingredients TEXT, method TEXT)")
+    #cur.execute("CREATE TABLE IF NOT EXISTS ltable(rname TEXT, ing TEXT, method TEXT, PRIMARY KEY (rname))")
     # Dinner Table = dtable
-    cur.execute("CREATE TABLE IF NOT EXISTS dtable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
+    #cur.execute("CREATE TABLE IF NOT EXISTS dtable(rname TEXT, ing TEXT, method TEXT, PRIMARY KEY (rname))")
     # Sweet/Dessert Table = stable
-    cur.execute("CREATE TABLE IF NOT EXISTS stable(name TEXT PRIMARY KEY, ingredients TEXT, methods TEXT)")
+    #cur.execute("CREATE TABLE IF NOT EXISTS stable(rname TEXT, ing TEXT, method TEXT, PRIMARY KEY (rname))")
     
     db.commit() # Save changes
     
     # Insert examples into tables
-    cur.execute("INSERT INTO btable VALUES('Pancakes', 'Eggs, Milk, SF Flour', 'Add all to bowl and mix.')")
-    cur.execute("INSERT INTO ltable VALUES('Cheese Sandwich', 'Bread, Butter, Cheese', 'Butter bread, add cheese')")
-    cur.execute("INSERT INTO dtable VALUES('Roast Chicken', 'Whole Chicken, Butter, Salt, Pepper', 'Put butter, salt and pepper on chicken')")
-    cur.execute("INSERT INTO stable VALUES('Cupcake', 'Eggs, Sugar, Marg, SF Flour', 'Add all to bowl and mix.')")
+    cur.execute("INSERT INTO btable (rname, ing, method) VALUES ('Pancakes', 'Eggs, Milk, SF Flour', 'Add all to bowl and mix.')")
+    #cur.execute("INSERT INTO ltable (rname, ing, method) VALUES('Cheese Sandwich', 'Bread, Butter, Cheese', 'Butter bread, add cheese')")
+    #cur.execute("INSERT INTO dtable (rname, ing, method) VALUES('Roast Chicken', 'Whole Chicken, Butter, Salt, Pepper', 'Put butter, salt and pepper on chicken')")
+    #cur.execute("INSERT INTO stable (rname, ing, method) VALUES('Cupcake', 'Eggs, Sugar, Marg, SF Flour', 'Add all to bowl and mix.')")
     
     db.commit()
     db.close() # Close database
