132d0ae15430ee880ee648cae65138dd884e0abb
rebeccabernie <rebecca.kane@outlook.com>
2016-11-28 13:47:39 +0000

    Not outputting contents of database to screen, changed /savedRecipes

--- recipekeeper.py
+++ recipekeeper.py
@@ -8,7 +8,7 @@ app = fl.Flask(__name__)
 def loadDB():
   db = getattr(fl.g, '_database', None)
   if db is None:
-    db = fl.g._database = sqlite3.connect(DATABASE)
+    db = fl.g._database = sqlite3.connect('recipes.db')
     return db
 
 @app.route("/")
@@ -16,12 +16,12 @@ def root():
     return app.send_static_file('index.html') # Load index.html file
 
 @app.route("/add", methods=["GET", "POST"])
-def bfast():
+def addRecipe():
     rname = fl.request.values["rname"]
     ing = fl.request.values["ing"]
     method = fl.request.values["method"]
 
-    db = sqlite3.connect(DATABASE) # Create connection object, represents database. Data stored in DATABASE, defined above
+    db = sqlite3.connect('recipes.db') # Create connection object, represents database. Data stored in DATABASE, defined above
     cur = db.cursor() # Allows SQL commands to be performed
 
     # Formatting for /VALUES(%s), var/ found at http://stackoverflow.com/questions/902408/how-to-use-variables-in-sql-statement-in-python
@@ -32,9 +32,9 @@ def bfast():
 
     return rname
 
-@app.route("/saved", methods=["GET", "POST"])
-def savedRecipes():
-    db = sqlite3.connect(DATABASE)
+@app.route("/savedRecipes", methods=["GET", "POST"])
+def savedRec():
+    db = sqlite3.connect('recipes.db')
     cur = db.cursor()
     
     cur.execute("SELECT rname FROM btable")
