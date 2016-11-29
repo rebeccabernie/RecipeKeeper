9427ad1d2e3bf95503b2134653a64952652ca618
rebeccabernie <rebecca.kane@outlook.com>
2016-11-29 16:10:20 +0000

    Now updating database with one field of user input, working on multiple

--- recipekeeper.py
+++ recipekeeper.py
@@ -1,45 +1,44 @@
 import flask as fl
 import sqlite3
 
-DATABASE = 'recipes.db'
 app = fl.Flask(__name__)
 
-# Load database, adapted from https://github.com/data-representation/example-sqlite
-def loadDB():
-  db = getattr(fl.g, '_database', None)
-  if db is None:
-    db = fl.g._database = sqlite3.connect('recipes.db')
-    return db
+DATABASE = 'recipes.db'
+connDB = sqlite3.connect(DATABASE)
+cur = connDB.cursor()
 
+# Load database and close connection adapted from https://github.com/data-representation/example-sqlite
+def loadDB():
+    db = getattr(fl.g, '_database', None)
+    if db is None:
+        db = fl.g._database = sqlite3.connect('recipes.db')
+    return db
+"""
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(fl.g, '_database', None)
+    if db is not None:
+        db.close()
+"""
 @app.route("/")
 def root():
     return app.send_static_file('index.html') # Load index.html file
-
+    cur.execute("SELECT rname FROM btable")
+    return str(cur.fetchall())
+    
 @app.route("/add", methods=["GET", "POST"])
 def addRecipe():
-    rname = fl.request.values["rname"]
-    ing = fl.request.values["ing"]
-    method = fl.request.values["method"]
-
-    db = sqlite3.connect('recipes.db') # Create connection object, represents database. Data stored in DATABASE, defined above
-    cur = db.cursor() # Allows SQL commands to be performed
-
-    # Formatting for /VALUES(%s), var/ found at http://stackoverflow.com/questions/902408/how-to-use-variables-in-sql-statement-in-python
-    cur.execute("INSERT INTO btable VALUES(?, ?, ?)", (rname, ing, method))
-
-    db.commit()
-    db.close() # Close database
-
-    return rname
-
+    #cur.execute("INSERT INTO ltable (rname, ing, method) VALUES('Cheese Sandwich', 'Bread, Butter, Cheese', 'Butter bread, add cheese')")
+    cur.execute("INSERT INTO btable(rname) VALUES(?)",(fl.request.form['rname'],))
+    connDB.commit()
+    return str(cur.fetchall())
+    
 @app.route("/savedRecipes", methods=["GET", "POST"])
 def savedRec():
-    db = sqlite3.connect('recipes.db')
-    cur = db.cursor()
     
     cur.execute("SELECT rname FROM btable")
-    db.close() # Close database
     return str(cur.fetchall())
+
 
 if __name__ == "__main__":
     app.run()
